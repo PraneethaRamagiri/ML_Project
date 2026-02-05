15220defd8f7c2780e4dff7297e1cee5942cb35f
PraneethsRamagiri <praneetharamagiri2006@gmail.com>
2026-02-05 19:01:27 +0530

    Loggings and Exception

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,21 @@
 import sys
+import src.logger
 import logging
 
+
+
+# This function creates a detailed error message
+# including file name, line number, and error description
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()    # exc_info()  gives 3 parameters ,, in which 1 and 2 are not that important, but 3rd parameter gives info about in which file error has occured and in which line and also error message
+
+    # exc_info() returns:
+    # (exception_type, exception_value, traceback)
+    # We only need traceback, so we ignore first two
+
+   
+    _,_,exc_tb = error_detail.exc_info()    # exc_info()  gives 3 parameters,,3rd parameter gives info about in which file error has occured and in which line and also error message
+   
+   # Get the file name where the error occurred
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script named [{0}] line number [{1}] and error message is [{2}]".format(
         file_name,
@@ -11,18 +24,26 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 
+# Custom exception class for better error handling
+
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
+
+        # Call the parent Exception class constructor
+
         super().__init__(error_message)
+
+        # Generate and store detailed error message
+
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
     
 
-# if __name__=='__main__':
-#     try:
-#         a = 1/0
-#     except Exception as e:
-#         logging.info("Logging has started")
-#         raise CustomException(e,sys)
+if __name__=='__main__':
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Logging has started")
+        raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,25 @@ import logging
 import os
 from datetime import datetime
 
+# Create a unique log file name using current date and time
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+# Create path for logs directory
+
+logs_path = os.path.join(os.getcwd(),"logs")
+
+# Create logs directory if it does not exist
+
 os.makedirs(logs_path,exist_ok=True)
 
+
+# Full path for the log file
 
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=LOG_FILE_PATH,     #where logs will be stored
     format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level = logging.INFO
 
@@ -18,5 +28,5 @@ logging.basicConfig(
 )
 
 
-# if __name__=="__main__":
-#     logging.info("Logging has started")
+if __name__=="__main__":
+    logging.info("Logging has started")
